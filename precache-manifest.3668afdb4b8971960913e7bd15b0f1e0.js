2dfcb7a9925de3060b35d334d3280f2395d3deda
Conan <wcyuan@users.noreply.github.com>
2019-09-22 23:13:38 -0400

    Updates

---
+++ precache-manifest.3668afdb4b8971960913e7bd15b0f1e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3bfb6a68a01d7dce9ef4140db73b63",
+    "url": "/mastermind-react-ts/index.html"
+  },
+  {
+    "revision": "3a272b4a1d3412427dfc",
+    "url": "/mastermind-react-ts/static/css/main.27e39b85.chunk.css"
+  },
+  {
+    "revision": "bdf8f93c10c8e3d64bea",
+    "url": "/mastermind-react-ts/static/js/2.7aac84e2.chunk.js"
+  },
+  {
+    "revision": "3a272b4a1d3412427dfc",
+    "url": "/mastermind-react-ts/static/js/main.b8575c97.chunk.js"
+  },
+  {
+    "revision": "a5a1119f7932c8700d35",
+    "url": "/mastermind-react-ts/static/js/runtime-main.02d32bfa.js"
+  }
+]);
